7668bc783be334195292d2f24d75d711210f3ba6
avbrotune <avbro@DESKTOP-3SA7DOP>
2023-06-26 23:20:16 +0700

    Correct project for review

--- backend/api/views.py
+++ backend/api/views.py
@@ -61,7 +61,7 @@ class CustomUserViewSet(UserViewSet):
     def get_permissions(self):
         if self.action in ('subscriptions', 'subscribe'):
             return [IsAuthenticated(), ]
-        return [AllowAny(),]
+        return [AllowAny(), ]
 
 
 class TagViewSet(viewsets.ReadOnlyModelViewSet):
