5455f3cf79f3face2ff6c33d9e4a41b9e5a9578f
avbrotune <avbro@DESKTOP-3SA7DOP>
2023-06-28 01:53:30 +0700

    ...

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -310,5 +310,6 @@ class SubscriptionSerializer(UserSerializer):
             "email",
             "id",
             "first_name",
-            "last_name"
+            "last_name",
+            "username"
         )
